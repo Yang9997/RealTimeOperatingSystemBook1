bed6cb1054abb747f9ad3b6ca1531f293e48b56e
Yang9997 <1974442074@qq.com>
2025-11-29 23:00:32 +0800

    3 commit: 2025/11/29

---
+++ code/ch01/0101.c
@@ -0,0 +1,14 @@
+// 伪代码
+void main() {
+    // 初始化代码
+    InitialiseSystem();
+
+    // 应用程序代码
+    while (true) {
+        AcquireTemperature();
+        ProcessTemperature();
+        CalculateControlAction();
+        SetActuatorPosition();
+        WaitForNextCycle();
+    }
+}
